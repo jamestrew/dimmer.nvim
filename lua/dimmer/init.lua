1e7c789ec5a3c992ba7bf545719cb28a4a9fb19c
James Trew <j.trew10@gmail.com>
2021-11-29 20:40:21 -0500

    feat: add toggle dimmer functionality

--- lua/dimmer/init.lua
+++ lua/dimmer/init.lua
@@ -3,6 +3,7 @@ local config = require("dimmer.config")
 local M = {}
 local state = {}
 state.overlays = {}
+state.active = true
 
 function M.setup(opts)
   config.set_defaults(opts)
@@ -26,6 +27,16 @@ end
 
 function M.get_state()
   return state
+end
+
+function M.toggle()
+  if state.active then
+    vim.notify("dimmer.nvim disabled")
+    state.active = false
+  else
+    vim.notify("dimmer.nvim enabled")
+    state.active = true
+  end
 end
 
 return M
