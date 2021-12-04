103d30ed8b6681e4de8dc6c40766c00982fe1520
James Trew <66286082+jamestrew@users.noreply.github.com>
2021-12-04 14:18:04 -0500

    Win enter/close fix (#17)
    * fix: valid_windows edge cases
    
    * fix: identify win_close issue cause
    
    * fix: opacity in state.overlays
    
    * move functions around
    
    * check window exists
    
    fixes #12

--- lua/dimmer/events.lua
+++ lua/dimmer/events.lua
@@ -8,19 +8,19 @@ function M.init_augroup()
     au!
     au FileType * call setbufvar('%', 'ft', &ft)
     au WinEnter,VimEnter,FileType * call v:lua.require'dimmer.events'.handle_event('win_enter')
-    au WinClosed * call v:lua.require'dimmer.events'.handle_event('win_close')
+    au WinClosed * call v:lua.require'dimmer.events'.handle_event('win_close', expand('<afile>'))
     augroup END
   ]])
 end
 
-function M.handle_event(event)
+function M.handle_event(event, ...)
   -- really hate this shit
   -- wanna wrap this in a HOF or something
   if state.active then
     if event == "win_enter" then
       ui.win_enter()
     elseif event == "win_close" then
-      ui.win_close()
+      ui.win_close(...)
     end
   end
 end
--- lua/dimmer/ui.lua
+++ lua/dimmer/ui.lua
@@ -21,7 +21,7 @@ local function set_window_dim(win_id, dim)
       "Normal:" .. HI_DIMMER
     )
     vim.api.nvim_win_set_option(overlay.overlay_id, WINBLEND, opacity)
-    state.overlays[win_id].winblend = opacity
+    state.overlays[win_id].winblend = dim and opacity or 0
   end
 end
 
@@ -29,16 +29,18 @@ local function valid_window(win_id, alt_win_id)
   local bufnr_ok, bufnr = pcall(vim.api.nvim_win_get_buf, alt_win_id)
   if bufnr_ok then
     local ft_ok, ft = pcall(vim.api.nvim_buf_get_var, bufnr, "ft")
-    if not ft_ok then
-      return false, "ft not set"
-    else
-      if vim.api.nvim_win_get_option(alt_win_id, "diff") then
-      elseif config.values.ft_ignore[ft] then
-      elseif alt_win_id ~= win_id then
-        return true, ""
+    if ft_ok then
+      if
+        not vim.api.nvim_win_get_option(alt_win_id, "diff")
+        and not config.values.ft_ignore[ft]
+        and alt_win_id ~= win_id
+      then
+        return true, "dimming"
       else
-        return true, "what?"
+        return false, "skipping"
       end
+    else
+      return false, "ft not set"
     end
   else
     return false, "bufnr does not exist"
@@ -47,14 +49,53 @@ end
 
 local function dim_others(win_id)
   for alt_win_id, _ in pairs(state.overlays) do
-    local valid, error_msg = valid_window(win_id, alt_win_id)
+    local valid, msg = valid_window(win_id, alt_win_id)
     if valid then
       set_window_dim(alt_win_id, true)
-      log.trace("dim_others dimming win_id: " .. alt_win_id)
-    else
-      log.trace("dim_others " .. error_msg .. " - win_id: " .. alt_win_id)
+    end
+    log.trace("dim_others win_id: " .. alt_win_id .. " msg: " .. tostring(msg))
+  end
+end
+
+local function window_exists(win_id)
+  for _, windows in ipairs(vim.fn.getwininfo()) do
+    if windows.winid == win_id then
+      return true
     end
   end
+  return false
+end
+
+local function create_overlay(win_id)
+  if state.overlays[win_id] and window_exists(win_id) then
+    return
+  end
+
+  if vim.api.nvim_win_get_config(win_id)["relative"] == "" then
+    local window = {}
+    window.config = M.win_config(win_id)
+    window.buf_id = vim.api.nvim_create_buf(false, true)
+    window.overlay_id = vim.api.nvim_open_win(
+      window.buf_id,
+      false,
+      window.config
+    )
+
+    log.trace(
+      "create_overlay win_id: " .. win_id .. "overlay_id: " .. window.overlay_id
+    )
+    state.overlays[win_id] = window
+    set_window_dim(win_id, true)
+  end
+end
+
+local function get_overlayed_win_id(overlay_id)
+  for win_id, window in pairs(state.overlays) do
+    if window.overlay_id == overlay_id then
+      return win_id
+    end
+  end
+  return -1
 end
 
 function M.init_highlight()
@@ -76,16 +117,6 @@ function M.win_config(win_id)
   }
 end
 
-function M.create_overlay(win_id)
-  log.trace("create_overlay -win_id: " .. win_id)
-  local window = {}
-  window.config = M.win_config(win_id)
-  window.buf_id = vim.api.nvim_create_buf(false, true)
-  window.overlay_id = vim.api.nvim_open_win(window.buf_id, false, window.config)
-  state.overlays[win_id] = window
-  set_window_dim(win_id, true)
-end
-
 function M.undim_window_all()
   log.trace("undim_window_all")
   for win_id, _ in pairs(state.overlays) do
@@ -97,24 +128,28 @@ function M.win_enter()
   log.trace("win_enter")
   local win_id = vim.api.nvim_get_current_win()
 
-  if not state.overlays[win_id] then
-    if vim.api.nvim_win_get_config(win_id)["relative"] == "" then
-      M.create_overlay(win_id)
-    end
-  end
-
+  create_overlay(win_id)
   set_window_dim(win_id, false)
   dim_others(win_id)
 end
 
-function M.win_close()
-  local win_id = vim.api.nvim_get_current_win()
+function M.win_close(win_id)
+  win_id = tonumber(win_id)
   local overlay = state.overlays[win_id]
   if overlay == nil then
     log.trace("win_close - no overlay win_id: " .. win_id)
+    local overlayed_id = get_overlayed_win_id(win_id)
+    if overlayed_id ~= -1 then
+      create_overlay(overlayed_id)
+    end
   else
     vim.api.nvim_win_close(overlay.overlay_id, false)
-    log.trace("win_close - overlay closed win_id: " .. win_id)
+    log.trace(
+      "win_close - overlay closed win_id: "
+        .. win_id
+        .. "overlay_id: "
+        .. overlay.overlay_id
+    )
     state.overlays[win_id] = nil
   end
 end
